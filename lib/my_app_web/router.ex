5d05b77494951ebf63471977689d981c7b7ba919
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2018-07-07 21:39:23 +0800

    Use cors plugin and register routes for signin

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -3,11 +3,20 @@ defmodule MyAppWeb.Router do
 
   pipeline :api do
     plug(:accepts, ["json"])
+
+    plug(
+      Corsica,
+      origins: "http://localhost:8080",
+      log: [rejected: :error, invalid: :warn, accepted: :debug],
+      allow_headers: ["content-type"],
+      allow_credentials: true
+    )
   end
 
   scope "/api", MyAppWeb do
     pipe_through(:api)
 
     resources("/users", UserController, except: [:new, :edit])
+    post("/users/sign_in", UserController, :sign_in)
   end
 end
